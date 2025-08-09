9b51c3ae19d022d1ce1e671209155c75124a3183
Aditya <anupama12sg@gmail.com>
2025-08-09 20:26:21 +0530

    chore: dynamicaaly render home component

--- movie-page/src/pages/Home.jsx
+++ movie-page/src/pages/Home.jsx
@@ -1,11 +1,19 @@
+import MovieCard from "../components/MovieCard"
+
 function Home() {
     const movies = [
-        { id: 1, title: "Devil Wears Prada", release_date: "2008"},
-        { id: 2, title: "Leap Year", release_date: "2016"},
-        { id: 3, title: "Hating Game", release_date: "2019"},
-        { id: 4, title: "Pocketful of Sunshine", release_date: "2020"},
-
+        { id: 1, title: "Devil Wears Prada", release_date: "2008" },
+        { id: 2, title: "Leap Year", release_date: "2016" },
+        { id: 3, title: "Hating Game", release_date: "2019" },
+        { id: 4, title: "Pocketful of Sunshine", release_date: "2020" },
     ]
 
-    return <div className="home"></div>
+    return <div className="home">
+        <div className="movie.grid">
+            {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
+
+            </div>
+    </div>
 }
+
+export default Home
