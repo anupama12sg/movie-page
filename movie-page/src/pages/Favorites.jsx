f9e0f829555acdfc3dc63dfeace833dad9c11eab
Aditya <anupama12sg@gmail.com>
2025-08-10 17:38:25 +0530

    chore: added code to favorites, installed react-router & used it

---
+++ movie-page/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h1>No Favorite Movies yet</h1>
+        <p>Start adding movies to your favorites & they will appear here</p>
+    </div>
+}
+
+export default Favorites
