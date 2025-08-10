40d13092937c3c35918720e7ee0c2c471e11b723
Aditya <anupama12sg@gmail.com>
2025-08-10 16:44:45 +0530

    chore: added button to the form

--- movie-page/src/pages/Home.jsx
+++ movie-page/src/pages/Home.jsx
@@ -15,6 +15,7 @@ function Home() {
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
             <input type="text" placeholder="Search for movies..." className="search-input" />
+            <button type="submit" className="search-button">Search</button>
         </form>
 
         <div className="movie.grid">
