80049053d14023f7800c1f5ee6ff295cd56cbb6e
Aditya <anupama12sg@gmail.com>
2025-08-10 16:58:52 +0530

    chore: useState

--- movie-page/src/pages/Home.jsx
+++ movie-page/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
+
     const movies = [
         { id: 1, title: "Devil Wears Prada", release_date: "2008" },
         { id: 2, title: "Leap Year", release_date: "2016" },
@@ -8,13 +11,15 @@ function Home() {
         { id: 4, title: "Pocketful of Sunshine", release_date: "2020" },
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("-------")
     }
 
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for movies..." className="search-input" />
+            <input type="text" placeholder="Search for movies..." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
             <button type="submit" className="search-button">Search</button>
         </form>
 
